5fe1a6bc75c26590907106eb2cd0c55e21dcaa84
thainguyen7116 <thainguyen7112006@gmail.com>
2026-01-26 22:54:26 +0700

    met

--- Teacher.cpp
+++ Teacher.cpp
@@ -30,3 +30,23 @@ void Teacher::showMenu(Database& db, User user) {
         }
     } while (choice != 0);
 }
+
+void Teacher::showClassMenu(Database& db, string classID) {
+    int choice;
+    do {
+        cout << "\n";
+        cout << "=== QUAN LY LOP: " << classID << " ===\n";
+        cout << "  [1]. TAO buoi diem danh moi\n";
+        cout << "  [2]. SUA / DONG MO diem danh\n";
+        cout << "  [3]. XEM BAO CAO diem danh\n";
+        cout << "  [0]. Quay lai Dashboard\n";
+        cout << ">> Lua chon cua ban: "; 
+        cin >> choice;
+
+        switch(choice) {
+            case 1: createSession(db, classID); break;
+            case 2: editSession(db, classID); break;
+            case 3: viewReport(db, classID); break;
+        }
+    } while (choice != 0);
+}
