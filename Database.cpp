524c8c5b3d941db7da85a1abb0326ccf6e0fb7d3
Anh Nhut <nhutlagiasd@gmail.com>
2026-01-28 18:12:56 +0700

    them thuoc tinh

--- Database.cpp
+++ Database.cpp
@@ -5,20 +5,17 @@
 
 using namespace std;
 
-vector<string> Database::split(string s, char d) {
+vector<string> Database::split(string str, char delimiter) {
     vector<string> tokens;
     string token;
-    stringstream ss(s);
+    stringstream ss(str);
 
-    while (getline(ss, token, d)) {
+    while (getline(ss, token, delimiter)) {
         tokens.push_back(token);
     }
     return tokens;
 }
 
-// =====================
-// Load all data
-// =====================
 void Database::loadAllData() {
     loadUsers();
     loadStudents();
@@ -27,27 +24,24 @@ void Database::loadAllData() {
     loadSessions();
     loadAttendance();
 
-    cout << ">> Da tai du lieu.\n";
+    cout << ">> Da tai du lieu xong.\n";
 }
 
-// =====================
-// Load functions (Read file)
-// =====================
 void Database::loadUsers() {
     users.clear();
-    ifstream f("users.txt");
+    ifstream file("users.txt");
     string line;
 
-    while (getline(f, line)) {
-        vector<string> d = split(line, '|');
-        if (d.size() >= 7) {
+    while (getline(file, line)) {
+        vector<string> data = split(line, '|');
+        if (data.size() >= 8) {
             users.push_back(User(
-                d[0], d[1], d[2],
-                d[3], d[4], d[5], d[6]
+                data[0], data[1], data[2], data[3],
+                data[4], data[5], data[6], data[7]
             ));
         }
     }
-    f.close();
+    file.close();
 }
 
 void Database::loadStudents() {
@@ -72,9 +66,7 @@ void Database::loadTeachers() {
     while (getline(f, line)) {
         vector<string> d = split(line, '|');
         if (d.size() >= 4) {
-            teachers.push_back(Teacher(
-                d[0], d[1], d[2], d[3]
-            ));
+            teachers.push_back(Teacher(d[0], d[1], d[2], d[3]));
         }
     }
     f.close();
@@ -88,9 +80,7 @@ void Database::loadClasses() {
     while (getline(f, line)) {
         vector<string> d = split(line, '|');
         if (d.size() >= 4) {
-            classes.push_back(ClassInfo(
-                d[0], d[1], d[2], d[3]
-            ));
+            classes.push_back(ClassInfo(d[0], d[1], d[2], d[3]));
         }
     }
     f.close();
@@ -123,59 +113,48 @@ void Database::loadAttendance() {
     while (getline(f, line)) {
         vector<string> d = split(line, '|');
         if (d.size() >= 4) {
-            attendances.push_back(Attendance(
-                d[0], d[1], d[2], d[3]
-            ));
+            attendances.push_back(Attendance(d[0], d[1], d[2], d[3]));
         }
     }
     f.close();
 }
 
-// =====================
-// Helper functions
-// =====================
 string Database::getStudentIDByUserID(string uID) {
-    for (int i = 0; i < students.size(); i++) {
+    for (int i = 0; i < students.size(); i++)
         if (students[i].getUserID() == uID)
             return students[i].getStudentID();
-    }
     return "";
 }
 
 string Database::getTeacherIDByUserID(string uID) {
-    for (int i = 0; i < teachers.size(); i++) {
+    for (int i = 0; i < teachers.size(); i++)
         if (teachers[i].getUserID() == uID)
             return teachers[i].getTeacherID();
-    }
     return "";
 }
 
 string Database::getUserIDByRoleID(string rID) {
-    for (int i = 0; i < students.size(); i++) {
+    for (int i = 0; i < students.size(); i++)
         if (students[i].getStudentID() == rID)
             return students[i].getUserID();
-    }
 
-    for (int i = 0; i < teachers.size(); i++) {
+    for (int i = 0; i < teachers.size(); i++)
         if (teachers[i].getTeacherID() == rID)
             return teachers[i].getUserID();
-    }
+
     return "";
 }
 
-// =====================
-// Save functions (Write file)
-// =====================
 void Database::saveSessions() {
     ofstream f("sessions.txt");
 
     for (int i = 0; i < sessions.size(); i++) {
         f << sessions[i].getSessionID() << "|"
-          << sessions[i].getClassID()   << "|"
-          << sessions[i].getTeacherID()<< "|"
-          << sessions[i].getDate()      << "|"
-          << sessions[i].getStartTime()<< "|"
-          << sessions[i].getEndTime()  << "|"
+          << sessions[i].getClassID() << "|"
+          << sessions[i].getTeacherID() << "|"
+          << sessions[i].getDate() << "|"
+          << sessions[i].getStartTime() << "|"
+          << sessions[i].getEndTime() << "|"
           << (sessions[i].getIsOpen() ? "1" : "0") << "|"
           << sessions[i].getPassword()
           << endl;
@@ -187,9 +166,9 @@ void Database::saveAttendance() {
     ofstream f("attendance.txt");
 
     for (int i = 0; i < attendances.size(); i++) {
-        f << attendances[i].getStudentID()  << "|"
-          << attendances[i].getSessionID()  << "|"
-          << attendances[i].getStatus()     << "|"
+        f << attendances[i].getStudentID() << "|"
+          << attendances[i].getSessionID() << "|"
+          << attendances[i].getStatus() << "|"
           << attendances[i].getCheckinTime()
           << endl;
     }
@@ -197,17 +176,18 @@ void Database::saveAttendance() {
 }
 
 void Database::saveUsers() {
-    ofstream f("users.txt");
+    ofstream file("users.txt");
 
     for (int i = 0; i < users.size(); i++) {
-        f << users[i].getUserID()   << "|"
-          << users[i].getUsername() << "|"
-          << users[i].getPassword() << "|"
-          << users[i].getFullName() << "|"
-          << users[i].getGender()   << "|"
-          << users[i].getPhone()    << "|"
-          << users[i].getRole()
-          << endl;
+        file << users[i].getUserID() << "|"
+             << users[i].getUsername() << "|"
+             << users[i].getPassword() << "|"
+             << users[i].getFullName() << "|"
+             << users[i].getDob() << "|"
+             << users[i].getGender() << "|"
+             << users[i].getPhone() << "|"
+             << users[i].getRole()
+             << endl;
     }
-    f.close();
+    file.close();
 }
